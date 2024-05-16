d3d51ec3488387f8d71e5d5d6e373c65a08580f7
JLCamachoHernandez <joseluiscamachohernandez4@gmail.com>
2024-05-16 14:45:34 -0700

    Boton de eliminar version 1

--- files/script.js
+++ files/script.js
@@ -2,6 +2,7 @@ let pokemones = [];
 let elementos = 0;
 
 document.getElementById('buscar').addEventListener('click', function(){
+
     elementos = elementos+1;
     if(elementos>3){
         swal('Numero maximo de pokemones','Favor de ingresar solo 3 pokemones en el quipo', 'info')
@@ -20,15 +21,16 @@ document.getElementById('buscar').addEventListener('click', function(){
 });
 
 document.getElementById('historial').addEventListener('click', function(){
-    const container1 = document.getElementById('pokemon-container');
+    const container1 = document.getElementById('pokemon-container1');
     const element1 = document.createElement('div');
+    element1.classList.add('page2');
         pokemones.forEach((pokemon1) => {
             element1.innerHTML += `
             <div>
                 <div>
                     <strong>Name:</strong> ${pokemon1.name}
-                    <img src=${pokemon1.sprites.front_default}>
                     <strong>Type:</strong> ${pokemon1.types[0].type.name}
+                    <img src=${pokemon1.sprites.front_default}>
                 <div>
             <div>
             `;
@@ -74,3 +76,8 @@ async function displayPokemon(pokemonName){
     pokemones.push(pokemon);
     console.log(pokemon);
 }
+
+document.getElementById('delete').addEventListener('click', function() {
+    document.getElementById('pokemon-container').innerHTML = '';
+    elementos.innerHTML == 0;
+});
